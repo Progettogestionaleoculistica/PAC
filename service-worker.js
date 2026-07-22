13e4136080d867d8e00680a9eac17ded6d4da575
Progettogestionaleoculistica <flaviogallo.oculista@gmail.com>
2026-07-22 23:18:51 +0200

    PWA structure - clean build

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,30 @@
-const CACHE_NAME = 'cache-v1';
+const CACHE_NAME = 'head-tilt-cache-v1';
 const urlsToCache = [
   '.',
-  'index.html',
-  'manifest.json',
-  'service-worker.js'
+  './index.html',
+  './manifest.json'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => Promise.all(
+      cacheNames.filter(name => name !== CACHE_NAME)
+                .map(name => caches.delete(name))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
